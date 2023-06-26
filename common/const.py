9836a09d8e16ed3d760a36259ebcaafc44275c2f
Oded <oded@permit.io>
2023-06-26 12:30:01 +0300

    update const

--- common/const.py
+++ common/const.py
@@ -1,4 +1,4 @@
 # consts
 emails_exclude_list = ['@permit.io1$', '^odedbd@gmail.com$']
 general_channel_names = ['general']
-public_channel_names = ['opal', 'permit-saas', 'cedar', 'test_permit_bot']
+public_channel_names = ['opal', 'permit-saas', 'cedar']
