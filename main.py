7eaa1612b5a0d501a89aed804c8eb9fd89767aaf
Oded <oded@permit.io>
2023-06-26 12:05:44 +0300

    disable community

--- main.py
+++ main.py
@@ -57,12 +57,12 @@ async def check_if_slack_retries(request: Request, call_next: Callable):
         return response
 
 
+# slack_event_manger = SlackEventManager(singing_secret=slack_signing_secret,
+#                                        endpoint='/support/slack_events',
+#                                        app=app)
+
 slack_event_manger_inc = SlackEventManager(singing_secret=slack_signing_secret_inc,
                                        endpoint='/support/slack_events_inc',
-                                       app=app)
-
-slack_event_manger = SlackEventManager(singing_secret=slack_signing_secret,
-                                       endpoint='/support/slack_events',
                                        app=app)
 
 pager_duty_api = APISession(
@@ -107,9 +107,9 @@ def add_message_to_cache(message_id):
 async def message_sent_inc(event_data):
     await _message_sent(event_data, _slack_client_inc)
 
-@slack_event_manger.on('message')
-async def message_sent(event_data):
-    await _message_sent(event_data, _slack_client)
+# @slack_event_manger.on('message')
+# async def message_sent(event_data):
+#     await _message_sent(event_data, _slack_client)
 
 async def _message_sent(event_data, slack_client):
 
